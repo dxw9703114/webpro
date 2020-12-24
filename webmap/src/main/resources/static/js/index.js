785f90f3f41754a6da3a31d2d080e01e38cf6a19
dengxiaowei <dengxiaowei@thunisoft.com>
2020-12-24 17:26:40 +0800

    feature:加载中国地图，添加鼠标移动事件和点击事件

--- webmap/src/main/resources/static/js/index.js
+++ webmap/src/main/resources/static/js/index.js
@@ -1,15 +1,47 @@
+
+var chinaSource = new ol.source.Vector({
+    projection: 'EPSG:3857',
+    url: "js/geojson/100000_full.json",
+    format: new ol.format.GeoJSON()
+});
+
+var chinaLayer = new ol.layer.Vector({
+    source: chinaSource,
+    zIndex: 1
+});
+
+var mvoeInteraction = new ol.interaction.Pointer({
+    handleMoveEvent: function (event) {
+        var box = document.getElementById("box");
+        var left = event.pointerEvent.clientX;
+        var top = event.pointerEvent.clientY;
+        var lonlat = ol.proj.transform(event.coordinate, 'EPSG:3857', 'EPSG:4326');
+        box.style.left = (left + 10) + 'px';
+        box.style.top = (top + 10) + 'px';
+        box.innerHTML = "经度: " + lonlat[0] + "<br>" + "纬度: " + lonlat[1];
+    },
+    handleDownEvent: function (event) {
+        console.log(event);
+        var feature = map.getFeaturesAtPixel(event.pixel);
+        if (feature) {
+            var featureYype = feature[0].getGeometryName();
+            var featureInfo = feature[0].getProperties();
+            var box = document.getElementById("box");
+            box.innerHTML = "类型: " + featureYype + "<br>名称: " + featureInfo.name + "<br>级别: " + featureInfo.level;
+        }
+    }
+});
+var singleClickInteraction = new ol.interaction.Select({
+    condition: ol.events.singleClick
+});
 var map = new ol.Map({
     target: 'map',
-    layers: [
-        new ol.layer.Vector({
-            source: new ol.source.Vector({
-                projection: 'EPSG:4326',
-                url: "js/geojson/china.js",
-                format: new ol.format.GeoJSON()
-            })
-        })],
+    layers: [chinaLayer],
     view: new ol.View({
-        center: ol.proj.fromLonLat([0, 0]),
-        zoom: 2
+        projection: ol.proj.get('EPSG:3857'),
+        center: ol.proj.fromLonLat([116.40, 39.90]),
+        zoom: 4
     })
 });
+map.addInteraction(mvoeInteraction);
+map.addInteraction(singleClickInteraction);
