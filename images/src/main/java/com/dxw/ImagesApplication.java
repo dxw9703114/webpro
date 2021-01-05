ae2462fd8e3209738d6624386d613969d76df66b
dengxiaowei <dengxiaowei@thunisoft.com>
2021-01-05 18:10:57 +0800

    feature:图片上传接口

---
+++ images/src/main/java/com/dxw/ImagesApplication.java
@@ -0,0 +1,16 @@
+package com.dxw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author dxw
+ * @version 1.1.0
+ * @date 2020/12/31 17:52
+ */
+@SpringBootApplication
+public class ImagesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ImagesApplication.class, args);
+    }
+}
